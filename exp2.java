1f6e089d135baeec975d48850962658721f7bb57
Kiran Kumar <63712150+kiran-kumarkk@users.noreply.github.com>
2022-02-27 12:53:14 +0530

    Add files via upload

---
+++ exp2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class exp2 {
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		 System.out.println("Enter the numbers : ");
+		 int x = s.nextInt();
+		 int y = s.nextInt();
+		 int a = x+y;
+		 System.out.println("Sum="+a);
+		 System.out.println("X="+x);
+		 System.out.println("Y="+y);
+	}
+}
